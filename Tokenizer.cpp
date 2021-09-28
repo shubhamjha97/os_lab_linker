76dd88379fe7190ee23df8302e9ece92d02ce475
Shubham Jha <sjha@shubhams-air.myfiosgateway.com>
2021-09-28 14:27:48 -0400

    Initial commit

---
+++ Tokenizer.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Shubham Jha on 9/28/21.
+//
+
+#include "Tokenizer.h"
+
+// TODO: Read lines from stdin
+// TODO: Read file line by line, keep track of current line, print current line
+
+class Tokenizer {
+    // TODO: Code tokenzier
+    // TODO: create a main class
+    // TODO: Write Makefile
+    // TODO: Add sample input
+
+public:
+    void getToken() {
+
+    }
+};
---
+++ Tokenizer.h
@@ -0,0 +1,16 @@
+//
+// Created by Shubham Jha on 9/28/21.
+//
+
+#ifndef LAB_1_TOKENIZER_H
+#define LAB_1_TOKENIZER_H
+
+
+
+class Tokenizer {
+
+};
+
+
+
+#endif //LAB_1_TOKENIZER_H
