e3864f6a3dbce7ad2a35c2493b3d6e5be51e3a4f
Shubham Jha <jha1shubham@gmail.com>
2021-09-29 02:33:01 -0400

    Comitting gitignore and main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include "Parser.cpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    string filename = "lab1_assign/"+(string) argv[1];
+    ifstream inFile(filename);
+
+    Parser parser = Parser(inFile);
+    parser.runPass1();
+    parser.runPass2();
+    cout<<"done"<<endl;
+    return 0;
+}
